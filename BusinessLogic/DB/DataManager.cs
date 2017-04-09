11f2fbbb8c0d3cecf4ffb618b20f417d866aec3a
ItAdvices <andrea.sba1993@gmail.com>
2017-04-09 19:31:06 +0200

    entity F Funzionante, finire ottimizzazione dataManager

---
+++ BusinessLogic/DB/DataManager.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLogic.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public static class DataManager<T> where T : class
+    {
+        const string dbService = "ITADVICES";
+        public static List<Utenti> getUsers()
+        {
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var res = (from u
+                               in db.Utenti
+                               select u).ToList();
+                    return res;
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+        public static T GetById(int id)
+        {
+            try
+            {
+                using (DBService<T> db = new DBService<T>())
+                {
+                    var result = db.Get(id);
+                    return result;
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+        public static T GetByGuid(Guid guid)
+        {
+            try
+            {
+                using (DBService<T> db = new DBService<T>())
+                {
+                    var result = db.Get(guid);
+                    return result;
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+        public static List<T> GetAll()
+        {
+            try
+            {
+                using (DBService<T> db = new DBService<T>())
+                {
+                    var result = db.GetAll();
+                    if (result != null)
+                        return result.ToList();
+                }
+            }
+            catch (Exception) { }
+            return null;
+        }
+    }
+}
