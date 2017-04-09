851acd30717a3c8433a1a64c253687f49da59a82
ItAdvices <andrea.sba1993@gmail.com>
2017-04-09 17:18:04 +0200

    b

---
+++ ITAdvices/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITAdvices
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblprova.Text = DataManager.getUsers().Count().ToString();
+        }
+    }
+}
