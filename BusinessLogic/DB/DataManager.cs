d1a5b59302666f2c204d9231241a0c6c0a2e8072
ItAdvices <andrea.sba1993@gmail.com>
2017-04-10 13:19:36 +0200

    d

--- BusinessLogic/DB/DataManager.cs
+++ BusinessLogic/DB/DataManager.cs
@@ -7,22 +7,6 @@ namespace BusinessLogic
 {
     public static class DataManager<T> where T : class
     {
-        const string dbService = "ITADVICES";
-        public static List<Utenti> getUsers()
-        {
-            try
-            {
-                using (DBEntities db = new DBEntities())
-                {
-                    var res = (from u
-                               in db.Utenti
-                               select u).ToList();
-                    return res;
-                }
-            }
-            catch (Exception) { }
-            return null;
-        }
         public static T GetById(int id)
         {
             try
