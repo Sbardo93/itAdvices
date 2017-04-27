773bafc08b9e8321f3a6c1daa29fac0e85c65b4a
ItAdvices <andrea.sba1993@gmail.com>
2017-04-27 12:31:19 +0200

    d

--- Entity/DB/DBContainer.Designer.cs
+++ Entity/DB/DBContainer.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ItAdvices\Dev\ItAdvices\Entity\DB\DBContainer.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ItAdvices\Dev\ITAdvices\Entity\DB\DBContainer.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
